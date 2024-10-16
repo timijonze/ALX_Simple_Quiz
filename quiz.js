e5ed5ff613fb7ac9d5344d927cd6bf7a22ea18af
timijonze <olutimehinalu@gmail.com>
2024-10-16 12:22:55 +0100

    add javaacript file

---
+++ quiz.js
@@ -0,0 +1,16 @@
+function checkAnswer() {
+    // Function body
+}
+
+function checkAnswer() {
+    let correctAnswer= "4";
+    let userAnswer= document.querySelector('input[name="quiz"]:checked').value;
+
+    if (userAnswer === correctAnswer) {
+        document.getElementById('feedback').textContent ="Correct! Well done.";
+    } else{
+        document.getElementById('feedback').textContent ="That's incorrect. Try again!";
+    }
+}
+
+document.getElementById('submit-answer').addEventListener('click', checkAnswer);
